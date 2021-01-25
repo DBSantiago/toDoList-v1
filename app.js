83fed604ba8b5bd28228a3744361b25bd62feef1
Santiago De Blasis <71677491+DBSantiago@users.noreply.github.com>
2021-01-25 19:48:31 -0300

    Added files

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  var day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req,res) {
+
+  var item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  };
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems:workItems});
+});
+
+app.post("/work", function(req, res){
+  var item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server up and running");
+});
